342de78bfb40a9697fea6f0026938b7a0a7edf85
Mafuyeka <tshepisoroshina@gmail.com>
2025-07-20 12:48:10 -0700

    Updated

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,23 @@
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Author, Book, Library, Librarian
 
 def list_books_in_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    if library:
-        return library.books.all()
-    return Book.objects.none()
-
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()  # assuming related_name='books' in Book model's FK to Library
+    except ObjectDoesNotExist:
+        return Book.objects.none()
 
 def books_by_author(author_name):
-    author = Author.objects.filter(name=author_name).first()
-    if author:
+    try:
+        author = Author.objects.get(name=author_name)
         return Book.objects.filter(author=author)
-    return Book.objects.none()
-
+    except ObjectDoesNotExist:
+        return Book.objects.none()
 
 def get_librarian_for_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
-    if library:
+    try:
+        library = Library.objects.get(name=library_name)
         return getattr(library, 'librarian', None)
-    return None
+    except ObjectDoesNotExist:
+        return None
