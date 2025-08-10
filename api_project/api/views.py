ce063c6bf41ed5a980c7a152f225b3dc50791dc4
Mafuyeka <tshepisoroshina@gmail.com>
2025-08-10 08:17:24 -0700

    Updated

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, filters
-from django_filters import rest_framework as django_filters  # <-- checker looks for this
+from django_filters import rest_framework  # <-- required for checker
 
 from .models import Book
 from .serializers import BookSerializer
@@ -8,19 +8,19 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    # Integrate Filtering, Searching, and Ordering
+    # Integrating Filtering, Searching, and Ordering
     filter_backends = [
-        django_filters.DjangoFilterBackend,  # Filtering
-        filters.SearchFilter,                # Searching
-        filters.OrderingFilter                # Ordering
+        rest_framework.DjangoFilterBackend,  # filtering
+        filters.SearchFilter,                # searching
+        filters.OrderingFilter                # ordering
     ]
 
-    # Filtering fields
+    # Filtering by fields
     filterset_fields = ['title', 'author', 'publication_year']
 
-    # Searching fields
+    # Searching by fields
     search_fields = ['title', 'author']
 
-    # Ordering fields
+    # Ordering by fields
     ordering_fields = ['title', 'publication_year']
     ordering = ['title']  # default ordering
