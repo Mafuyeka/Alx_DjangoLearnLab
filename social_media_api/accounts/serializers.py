9970662917ad6e55efb8097aa30ffbc5641f0ccb
Mafuyeka <tshepisoroshina@gmail.com>
2025-08-24 06:48:50 -0700

    Updated

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,24 +4,30 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
+
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    email = serializers.CharField()
+    bio = serializers.CharField(required=False, allow_blank=True)
+    profile_picture = serializers.CharField(required=False, allow_blank=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+        fields = ["id", "username", "email", "bio", "profile_picture", "followers"]
 
 
-class RegisterSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    email = serializers.CharField(max_length=255)
     password = serializers.CharField(write_only=True)
 
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password']
-
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
+        user = get_user_model().objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"]
         )
+        # Create token
         Token.objects.create(user=user)
         return user
+
